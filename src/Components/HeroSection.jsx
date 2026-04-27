ee795a6e5c0b8492064ea079f970f6e06e1f1561
shuddy05 <horlabodehyibrahim@gmail.com>
2026-04-27 01:09:15 +0100

    new commit

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,36 +1,57 @@
-import React from 'react'
-import image from "../assets/images/image 489.png"
+import React from "react";
+import image from "../assets/images/image 489.png";
+
 const HeroSection = () => {
   return (
-    <main className='h-screen'>
-      <div className="layout flex justify-between">
-    <div>
+    <main className="min-h-screen bg-[#f5f5e8] flex items-center">
+      <div className="layout grid lg:grid-cols-2 gap-12 items-stretch order-2 lg:order-1 ">
+        <div className="flex flex-col justify-between h-full">
+          <div>
+            <h1 className="font-bold text-5xl leading-tight mb-6">
+              Buy, Rent or Sell Your Property Easily
+            </h1>
 
-      <h1> Buy, Rent or Sell Your Property Easily</h1>
-      <p>A great platform to buy, sell, or even rent your properties without any commissions.</p>
+            <p className="text-lg text-gray-500 mb-10">
+              A great platform to buy, sell, or even rent your properties
+              without any commissions.
+            </p>
 
-      <button type="button" className='text-white bg-purple-500 rounded-xl'> Browse Properties</button>
-    <div className='flex justify-between gap-10'>
-      <div className='flex flex-col gap-6'><h1>50K+</h1>
-      <p>Happy Renters</p></div>
-       <div className='flex flex-col gap-6'><h1>100K+</h1>
-      <p>Active Users</p></div>
-       <div className='flex flex-col gap-6'><h1>1K+</h1>
-      <p>Properties Listed</p></div>
-    </div>
+            <button
+              type="button"
+              className="text-white text-lg bg-purple-500 rounded-lg py-4 px-8"
+            >
+              Browse Properties
+            </button>
+          </div>
 
-    </div>
-    <div>
+          <div className="grid grid-cols-3 gap-8 mt-16">
+            <div>
+              <h2 className="text-4xl font-semibold">50K+</h2>
+              <p className="text-[#403F3F] text-lg">Happy Renters</p>
+            </div>
 
+            <div>
+              <h2 className="text-4xl font-semibold">100K+</h2>
+              <p className="text-[#403F3F] text-lg">Active Users</p>
+            </div>
 
-      <img src={image} alt="" />
-    </div>
+            <div>
+              <h2 className="text-4xl font-semibold">1K+</h2>
+              <p className="text-[#403F3F] text-lg">Properties Listed</p>
+            </div>
+          </div>
+        </div>
 
+        <div className="h-full order-1 lg:order-2 ">
+          <img
+            src={image}
+            alt="Luxury property"
+            className="w-full h-full object-cover rounded-3xl"
+          />
+        </div>
       </div>
-
-
     </main>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-
-
+import React from "react";
+import HeroSection from "../Components/HeroSection";
+import FeaturesProperties from "../Components/FeaturesProperties";
+import HomeComponents from "../Components/HomeComponents";
 const Home = () => {
   return (
     <main>
-      div
-      
+      <HeroSection />
+      <FeaturesProperties />
+      <HomeComponents />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
